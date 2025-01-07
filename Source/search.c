c24bc8294f8b58063ef24d417e9a5ad1cfffbcac
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2025-01-07 13:31:59 +0100

    search.c: QS Check Evasions
    bench: 3385272

--- Source/search.c
+++ Source/search.c
@@ -454,8 +454,18 @@ static inline int quiescence(position_t *pos, thread_t *thread,
   // create move list instance
   moves move_list[1];
 
+  // is king in check
+  int in_check = is_square_attacked(pos,
+                                    (pos->side == white)
+                                        ? __builtin_ctzll(pos->bitboards[K])
+                                        : __builtin_ctzll(pos->bitboards[k]),
+                                    pos->side ^ 1);
+  if (in_check) {
+    generate_moves(pos, move_list);
+  } else {
   // generate moves
-  generate_captures(pos, move_list);
+    generate_captures(pos, move_list);
+  }
 
   for (uint32_t count = 0; count < move_list->count; count++) {
     score_move(pos, thread, ss, &move_list->entry[count], best_move);
